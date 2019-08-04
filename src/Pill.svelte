fe54423eeb6706f644c4a2b849a81ffc531b664f
Mathieu Anderson <mathieu.hj.anderson@gmail.com>
2019-08-04 19:12:10 +0200

    Add Pill component

---
+++ src/Pill.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let name;
+  export let icon;
+  export let color;
+
+  let backgroundColor = `${color.slice(0, 11)}95% `;
+</script>
+
+<style>
+  div {
+    padding: 0.3em;
+    margin: 0.5em;
+    width: min-content;
+  }
+</style>
+
+<div
+  style="border: 2px solid hsl({color}); background-color: hsl({backgroundColor});
+  color: hsl({color}); box-shadow: 4px 6px 0px hsl({color});">
+  {icon}{name}
+</div>
