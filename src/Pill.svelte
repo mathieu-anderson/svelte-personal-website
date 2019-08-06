764474ed608130771d0a92a0780ac0cbc535b50b
Mathieu Anderson <mathieu.hj.anderson@gmail.com>
2019-08-06 18:42:42 +0200

    Add PoC icon in PIll component

--- src/Pill.svelte
+++ src/Pill.svelte
@@ -8,14 +8,21 @@
 
 <style>
   div {
-    padding: 0.3em;
+    padding: 0.3em 0.3em 0.2em 0.3em;
     margin: 0.5em;
-    width: min-content;
+    display: flex;
+    flex-flow: row nowrap;
+  }
+  .name {
+    margin-left: 0.3em;
   }
 </style>
 
 <div
   style="border: 2px solid hsl({color}); background-color: hsl({backgroundColor});
   color: hsl({color}); box-shadow: 4px 6px 0px hsl({color});">
-  {icon}{name}
+  <span class="icon">
+    {@html icon}
+  </span>
+  <span class="name">{name}</span>
 </div>
