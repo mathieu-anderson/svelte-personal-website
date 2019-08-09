2ffe186cffd22630669ad8462f7554da4b386fcb
Mathieu Anderson <mathieu.hj.anderson@gmail.com>
2019-08-09 15:33:01 +0200

    Add info on click of PIll

--- src/Pill.svelte
+++ src/Pill.svelte
@@ -4,10 +4,12 @@
   export let color;
   export let darkTheme;
   export let showPillInfo;
+  export let pillInfo;
 
   $: scale = !showPillInfo;
   $: boxColor = darkTheme ? "0, 0%, 20%" : color;
   $: fontColor = darkTheme ? "0, 0%, 86%" : color;
+  $: infoFontColor = darkTheme ? "0, 0%, 86%" : "#333";
   $: backgroundColor = darkTheme
     ? "0, 0%, 0%"
     : `${color.slice(0, color.length - 3)}98% `;
@@ -21,8 +23,9 @@
     display: flex;
     flex-flow: row nowrap;
     transition: 150ms;
-    height: 1.2em;
-    width: fit-content;
+    height: auto;
+    width: auto;
+    font-weight: bold;
   }
   .scale:hover {
     transform: scale(1.03);
@@ -30,6 +33,11 @@
   .name {
     margin-left: 0.3em;
     line-height: 1.1em;
+  }
+
+  .pill-info {
+    margin-left: 1em;
+    color: #333;
   }
 </style>
 
@@ -43,4 +51,9 @@
     {@html icon}
   </div>
   <div class="name">{name}</div>
+  <div class="pill-info" style="color: hsl({infoFontColor})">
+    {#if showPillInfo}
+      {@html pillInfo}
+    {/if}
+  </div>
 </div>
